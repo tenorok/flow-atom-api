4fda77b77e667dc6f79b117f0ee8c3330f4eed91
travis <travis@browser.html>
2016-07-09 17:42:20 -0600

    Add declarations for "atom" module

--- sig/v1.7.4/index.js
+++ sig/v1.7.4/index.js
@@ -1,3 +1,16 @@
 // @flow
 
 declare var atom: AtomEnvironment;
+
+declare module "atom" {
+  declare module.exports: {
+    CompositeDisposable: Class<EventKit$CompositeDisposable>;
+    Directory: Class<Pathwatcher$Directory>;
+    Disposable: Class<EventKit$Disposable>;
+    Emitter: Class<EventKit$Emitter>;
+    File: Class<Pathwatcher$File>;
+    Point: Class<TextBuffer$Point>;
+    Range: Class<TextBuffer$Range>;
+    TextBuffer: Class<TextBuffer$TextBuffer>;
+  };
+}
